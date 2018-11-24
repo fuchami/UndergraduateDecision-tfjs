875793a90f298106e345c06ab0c3c8fcfb687a56
fuchami <famichiki.yuuki@gmail.com>
2018-11-24 19:20:48 +0900

    update

--- src/components/top.vue
+++ src/components/top.vue
@@ -3,7 +3,9 @@
     <h1>{{ msg }}</h1>
 
     <div>
-      <video ref="video" id="video" width="640" height="480" autoplay></video>
+      <v-flex>
+      <video ref="video" id="video" width="256" height="256" autoplay></video>
+      </v-flex>
     </div>
 
   </div>
@@ -16,16 +18,18 @@ export default {
     return {
       msg: '学部診断アプリ',
       video: {},
+      medias: {},
       canvas: {},
       captures: []
     }
   },
   mounted () {
     // このアプリがロードされたときに起動
+    this.medias = {audio: false, video: true}
     this.video = this.$refs.video
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-        this.video.src = window.URL.createObjectURL(stream)
+        this.video.srcObject = stream
         this.video.play()
       })
     }
