641299bad71286f457837363a30ba8db62739d38
fuchami <famichiki.yuuki@gmail.com>
2018-11-24 18:57:22 +0900

    build try

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: '',
-    assetsPublicPath: '',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
